25f5e40c6142de98f0e494b7e0b88dbc380146b5
Alfredo Ormeno <ormenoandres13@gmail.com>
2023-08-21 20:18:23 -0400

    Feat: Add comparable module to Node class
    This commit adds the comparable module to the node class in order to
    allow the comparison of nodes using their data attribute.
    
    The #<=>(other) method was added to facilitate node data comparison.

--- node.rb
+++ node.rb
@@ -1,7 +1,15 @@
-class NODE
+class Node
+  include Comparable
+  attr_accessor :data, :left, :right
+
   def initialize(data = nil , left = nil, right = nil)
-    @root = data
-    @left_child = left
-    @right_child = right
+    @data = data
+    @left = left
+    @right = right
+  end
+
+  def <=>(other)
+    value = other.class == Node ? other.data : other
+    data <=> value
   end
 end
